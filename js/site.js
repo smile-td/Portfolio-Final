82aa30c75a17b7e05d7fac6cc16724af2a2c5018
Smile-td <dengy087@newschool.edu>
2020-12-07 23:25:09 +0800

    add airtable.browser.js

---
+++ js/site.js
@@ -0,0 +1,22 @@
+var Airtable = require('airtable');
+var base = new Airtable({apiKey: 'YOUR_API_KEY'}).base('app611tk7BYrA0HTQ');
+
+base('Case Study Table').select({
+    // Selecting the first 3 records in Grid view:
+    maxRecords: 3,
+    view: "Grid view"
+}).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
+
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('WorkTitle'));
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+}, function done(err) {
+    if (err) { console.error(err); return; }
+});
